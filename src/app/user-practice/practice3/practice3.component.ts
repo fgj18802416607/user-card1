220ab1d243e9e444e5f47617670f5368666d7bf3
18802416607 <18802416607@163.com>
2018-12-13 16:26:46 +0800

    time

--- src/app/user-practice/practice3/practice3.component.ts
+++ src/app/user-practice/practice3/practice3.component.ts
@@ -1,22 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-// import { Observable, Subject } from 'rxjs';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'app-practice3',
   templateUrl: './practice3.component.html',
   styleUrls: ['./practice3.component.css']
 })
+
 export class Practice3Component implements OnInit {
 
   public today;
-
   constructor() { }
-  ngOnInit() {
-    let today1 = new Date().toLocaleDateString();
-    let today2 = new Date().toTimeString().slice(0,8);
-    this.today = today1 +" "+ today2;
-    console.log(today1);
-    console.log(today2);
-  }
 
+  ngOnInit() {
+    // let today1 = new Date().toLocaleDateString();
+    // let today2 = new Date().toTimeString().slice(0, 8);
+    // this.today = today1 + " " +today2;
+    //secondsCounter
+    this.today =new Date();
+    const timeNow = interval(1000);
+    timeNow.subscribe(n => this.today = new Date());
+  }
 }
+
--- src/app/user-practice/user-practice.component.ts
+++ src/app/user-practice/user-practice.component.ts
@@ -10,5 +10,4 @@ export class UserPracticeComponent implements OnInit {
 
   ngOnInit() {
   }
-
 }
