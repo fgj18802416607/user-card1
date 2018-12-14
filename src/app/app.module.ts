597d5466d60e52a7e0989f42f1038e78638de34e
18802416607 <18802416607@163.com>
2018-12-14 11:44:02 +0800

    practice3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,8 +13,6 @@ import { MultiplePipe } from './pipe/multiple.pipe';
 import { Practice1Component } from './user-practice/practice1/practice1.component';
 import { Practice2Component } from './user-practice/practice2/practice2.component';
 import { Practice3Component } from './user-practice/practice3/practice3.component';
-import { Practice4Component } from './practice4/practice4.component';
-
 
 @NgModule({
   declarations: [
@@ -27,7 +25,6 @@ import { Practice4Component } from './practice4/practice4.component';
     Practice1Component,
     Practice2Component,
     Practice3Component,
-    Practice4Component
   ],
   imports: [
     BrowserModule,
